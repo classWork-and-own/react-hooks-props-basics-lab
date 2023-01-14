c8b5a1eb2bc4a4975dfae3c59e1cd490c74453c7
mukandalee <leonardmukanda254@gmail.com>
2023-01-14 14:15:51 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
+import Links from "./Links";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
@@ -12,7 +13,7 @@ function App() {
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About bio="i am a full stack developer"></About>
     </div>
   );
 }
